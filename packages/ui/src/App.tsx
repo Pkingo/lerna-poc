91193324b3e3279621d37567113c058739334710
Philip Andreas Kingo <philipandreaskingo@hotmail.com>
2021-09-22 12:43:37 +0200

    added some more cross dependencies

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import { Typography, Button, Stack } from "@lenus/components";
 import { Receipt, ReminderIcon } from "@lenus/icons";
+import { User } from "@lenus/types";
+import { isValidEmail } from "@lenus/utils";
 
 function App() {
+  const user: User = {
+    email: "email@test.com",
+    firstName: "Agent",
+    lastName: "Smith ",
+  };
   return (
     <Stack>
       <Typography variant="h1">Lerna POX</Typography>
@@ -10,7 +17,12 @@ function App() {
       <Typography variant="body1">And Icons from @lenus/components</Typography>
       <Receipt />
       <ReminderIcon />
-      <Button variant="outlined" color="primary">
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <Button
+        onClick={() => console.log(isValidEmail(user.email))}
+        variant="outlined"
+        color="primary"
+      >
         Click me!!
       </Button>
     </Stack>
